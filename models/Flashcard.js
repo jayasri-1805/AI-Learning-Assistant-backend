8be36df8c1326d50789c091c32ff1b4e0335b4af
Yaswanth Varada <yaswanthvarada20@gmail.com>
2026-01-19 23:51:22 +0530

    Some changes done

--- models/Flashcard.js
+++ models/Flashcard.js
@@ -1,43 +1,42 @@
-import { DiffIcon, Lasso } from "lucide-react";
 import mongoose from "mongoose";
 
 const flashcardSchema = new mongoose.Schema(
-    {
-        userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        documentId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Document",
-            required: true,
-        },
-        cards: [
-            {
-                question: { type: String, required: true },
-                answer: { type: String, required: true, },
-                Difficulty: {
-                    type: String,
-                    enum: ["easy", "medium", "hard"],
-                    default: "medium",
-                },
-                LastReviewed: {
-                    type: Date,
-                    default: null,
-                },
-                ReviewCount: {
-                    type: Number,
-                    default: 0,
-                },
-                isStarred: {
-                    type: Boolean,
-                    default: false,
-                },
-            },
-        ],
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    { timestamps: true }
+    documentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Document",
+      required: true,
+    },
+    cards: [
+      {
+        question: { type: String, required: true },
+        answer: { type: String, required: true },
+        Difficulty: {
+          type: String,
+          enum: ["easy", "medium", "hard"],
+          default: "medium",
+        },
+        LastReviewed: {
+          type: Date,
+          default: null,
+        },
+        ReviewCount: {
+          type: Number,
+          default: 0,
+        },
+        isStarred: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
 );
 
 flashcardSchema.index({ userId: 1, documentId: 1 });
